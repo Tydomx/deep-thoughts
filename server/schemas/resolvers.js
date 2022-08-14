ddc65bd92766bcd5f9742f9a10c5aead0fede0c3
Tydomx <michaelthai16@gmail.com>
2022-08-14 10:14:02 -0500

    Added thoughts and users to query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,30 @@
+// importing Thought/User models
+const { User, Thought } = require('../models');
+
 const resolvers = {
 	Query: {
-		helloWorld: () => {
-			return 'Hello World!';
+		// query thoughts, perform a find() method on Thought model, returning thought data in descending order
+		thoughts: async (parent, { username }) => {
+			const params = username ? { username } : {};
+			return Thought.find(params).sort({ createdAt: -1 });
+		},
+		// finding a single thought
+		thought: async (parent, { _id }) => {
+			return Thought.findOne({ _id });
+		},
+		// get all users
+		users: async () => {
+			return User.find()
+				.select('-__v -password')
+				.populate('friends')
+				.populate('thoughts');
+		},
+		// get a user by username
+		user: async (parent, { username }) => {
+			return User.findOne({ username })
+				.select('-__v -password')
+				.populate('friends')
+				.populate('thoughts');
 		}
 	}
 };
