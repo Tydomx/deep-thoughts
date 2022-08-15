7302c6482d9d3fd305a8606106ea288186fb259c
Tydomx <michaelthai16@gmail.com>
2022-08-15 18:03:51 -0500

    Created thought form to have text, text limit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 import ThoughtList from '../components/ThoughtList';
+
 
 import Auth from '../utils/auth';
 // allows us to make requests to GraphQL server we connected to and made available to app using ApolloProvder component on App.js
@@ -24,6 +26,11 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
+        {loggedIn && (
+          <div className='col-12 mb-3'>
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
